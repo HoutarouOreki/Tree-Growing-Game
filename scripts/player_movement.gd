9bb6e7b5e3be83d685331d5c864c3dafa47d80ae
HoutarouOreki <thezjarek@gmail.com>
2023-08-12 18:11:46 +0200

    basic tree planting

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 2.8
-const ACCELERATION = 0.5
+const ACCELERATION = 30.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -15,14 +15,14 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = move_toward(velocity.x, direction.x * SPEED, ACCELERATION)
-		velocity.z = move_toward(velocity.z, direction.z * SPEED, ACCELERATION)
-	else:
-		velocity.x = move_toward(velocity.x, 0, ACCELERATION)
-		velocity.z = move_toward(velocity.z, 0, ACCELERATION)
+	
+	var new_velocity = velocity.move_toward(direction * SPEED if direction else Vector3.ZERO, ACCELERATION * delta)
+	#if direction:
+		#new_velocity = velocity.move_toward(, ACCELERATION * delta)
+		
+	velocity.x = new_velocity.x
+	velocity.z = new_velocity.z
 
 	move_and_slide()
---
+++ scripts/tree_plant.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if Input.is_action_just_pressed("use_item"):
+		var camera = get_viewport().get_camera_3d()
+		var mousePos = get_viewport().get_mouse_position()
+		var rayLength = 3
+		var rayQuery = PhysicsRayQueryParameters3D.new()
+		rayQuery.from = camera.project_ray_origin(mousePos)
+		rayQuery.to = rayQuery.from + camera.project_ray_normal(mousePos) * rayLength
+		var space = get_world_3d().direct_space_state
+		var result = space.intersect_ray(rayQuery)
+		print(result)
+		if !result.is_empty():
+			on_plant(result["position"])
+
+func on_plant(position: Vector3):
+	var scene_resource = load("res://assets/models/trees/TreeStage0.glb")
+	var instance = scene_resource.instantiate() as Node3D
+	instance.position = position
+	get_tree().root.add_child(instance)
