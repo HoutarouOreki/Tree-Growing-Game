82e6639ab494eaac087b793ce5509c22e30aa625
HoutarouOreki <thezjarek@gmail.com>
2023-09-28 19:00:35 +0200

    update beehave

--- addons/beehave/nodes/composites/sequence_reactive.gd
+++ addons/beehave/nodes/composites/sequence_reactive.gd
@@ -15,10 +15,10 @@ func tick(actor: Node, blackboard: Blackboard) -> int:
 	for c in get_children():
 		if c.get_index() < successful_index:
 			continue
-		
+
 		if c != running_child:
 			c.before_run(actor, blackboard)
-		
+
 		var response = c.tick(actor, blackboard)
 		if can_send_message(blackboard):
 			BeehaveDebuggerMessages.process_tick(c.get_instance_id(), response)
@@ -26,7 +26,7 @@ func tick(actor: Node, blackboard: Blackboard) -> int:
 		if c is ConditionLeaf:
 			blackboard.set_value("last_condition", c, str(actor.get_instance_id()))
 			blackboard.set_value("last_condition_status", response, str(actor.get_instance_id()))
-		
+
 		match response:
 			SUCCESS:
 				successful_index += 1
@@ -51,7 +51,7 @@ func tick(actor: Node, blackboard: Blackboard) -> int:
 func interrupt(actor: Node, blackboard: Blackboard) -> void:
 	_reset()
 	super(actor, blackboard)
-	
+
 func _reset() -> void:
 	successful_index = 0
 
